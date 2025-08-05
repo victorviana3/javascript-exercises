b910f2ec04ab0fe4c2abeb016380fd58928e325e
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-08-05 16:21:31 -0300

    Exercise 11 - Temperature Conversion done.
    Made trying to keep readability at finest

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  let celsius = (inputTemp- 32) * 5/9
+  celsius = Number(celsius.toFixed(1))
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+   let fahrenheit = (inputTemp * (9/5)+32)
+  fahrenheit = Number(fahrenheit.toFixed(1))
+  return fahrenheit
 };
 
 // Do not edit below this line
