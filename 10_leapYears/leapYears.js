84bf8af81ae96d9038222de32e8b23b63b8c8c29
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-08-04 21:24:42 -0300

    Few more exercises completed
    Next goal is to improve legibility and maintainability

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year % 4 != 0) return false
+    if (year % 100 == 0 && year % 400 != 0) return false
+    if (year % 100 == 0 && year % 400 == 0) return true
+    return true
 };
 
 // Do not edit below this line
