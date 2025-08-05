a3f53f0c777d9ca0fa1bd7e9d1bb7793523534b9
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-08-05 12:21:47 -0300

    Exercise 10 completed
    After completion of the 16 exercises I'll try to rewrite them focusing on readability

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstOperator, secondOperator) {
+    let sum =0
+    if (firstOperator > 0 && Number.isInteger(firstOperator)){
+        if(secondOperator > 0 && Number.isInteger(secondOperator)){
+            if (secondOperator < firstOperator){
+        const temp = firstOperator
+        firstOperator = secondOperator
+        secondOperator = temp
+    }
+    for (i=firstOperator; i <= secondOperator; i++){
+        sum +=i
+    }
+    return sum;
+        }
+    }
+    return "ERROR"
+    
 };
 
 // Do not edit below this line
