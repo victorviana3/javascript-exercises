b4ecec0b507f216137a03b7728992241e0b397d0
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-08-05 16:54:12 -0300

    Exercise 12 - calculator completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (addendOne, addendTwo) {
+  return addendOne + addendTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function (minuend, subtrahend) {
+  return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let resultSum = 0;
+  array.forEach((item) => {
+    resultSum += item;
+  });
+  return resultSum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let resultMultiply = 1;
+  array.forEach((item) => {
+    resultMultiply *= item;
+  });
+  return resultMultiply;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  const resultPower = base ** exponent;
+  return resultPower;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) return 1;
+  let resultFactorial = number;
+  for (let i = number - 1; i >= 1; i--) {
+    resultFactorial *= i;
+  }
+  return resultFactorial;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
