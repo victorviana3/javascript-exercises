ccd9e5e7e168a6f75163867a6c037e05f4f59075
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-08-06 10:20:38 -0300

    Exercise 14 - Fibonacci completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (inputNumber) {
+  const memberNumber = Number(inputNumber);
+  if (memberNumber < 0) return "OOPS";
+  let fibonacci = [0, 1];
+  for (let i = 1; i < memberNumber; i++) {
+    fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
+  }
+  return fibonacci[memberNumber];
 };
 
 // Do not edit below this line
