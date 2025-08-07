c6ca051c2f3b9f661cd679e184561638e87d4cf0
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-08-07 14:22:09 -0300

    All exercises finished
    Still missing optimizations in many questions like 16, which I did not like my solution.
    
    Another day I'll come back to improve my code

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let maxAge = 0;
+  const thisYear = new Date().getFullYear();
+  people.forEach((person) => {
+    if (
+      maxAge <
+      (person.yearOfDeath ? person.yearOfDeath : thisYear) - person.yearOfBirth
+    )
+      maxAge =
+        (person.yearOfDeath ? person.yearOfDeath : thisYear) -
+        person.yearOfBirth;
+  });
+  return people.find(
+    (person) =>
+      (person.yearOfDeath ? person.yearOfDeath : thisYear) -
+        person.yearOfBirth ===
+      maxAge
+  );
 };
 
 // Do not edit below this line
