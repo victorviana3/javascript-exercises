96005a71c27a37232a9268aa3ab8d457f09ec290
Victor Viana <29336414+victorviana3@users.noreply.github.com>
2025-08-10 09:44:10 -0300

    First round of code improvement
    Increased readability in findTheOldest algorithm, while improving a bit the code by not
    running the array twice

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,21 +1,16 @@
 const findTheOldest = function (people) {
   let maxAge = 0;
   const thisYear = new Date().getFullYear();
+  let oldestPerson;
   people.forEach((person) => {
-    if (
-      maxAge <
-      (person.yearOfDeath ? person.yearOfDeath : thisYear) - person.yearOfBirth
-    )
-      maxAge =
-        (person.yearOfDeath ? person.yearOfDeath : thisYear) -
-        person.yearOfBirth;
+    let finalYear = person.yearOfDeath || thisYear;
+    let age = finalYear - person.yearOfBirth;
+    if (maxAge < age) {
+      maxAge = age;
+      oldestPerson = person;
+    }
   });
-  return people.find(
-    (person) =>
-      (person.yearOfDeath ? person.yearOfDeath : thisYear) -
-        person.yearOfBirth ===
-      maxAge
-  );
+  return oldestPerson;
 };
 
 // Do not edit below this line
